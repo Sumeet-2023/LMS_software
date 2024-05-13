40066452729d58c8accdca1b42a49bea21b9f25f
Rajkumar Pambhar <go72jon.mytum.de>
2024-05-13 18:01:00 +0200

    Implemented one fuctionality of menu

--- src/prototype/Main.java
+++ src/prototype/Main.java
@@ -8,12 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner;
 
-        System.out.println("Welcome to the Command Line Interface!");
         CLIRunner cliRunner = new CLIRunner();
         while (true) {
+            cliRunner.printOptions();
             scanner = new Scanner(System.in);
             cliRunner.getPrompter().setScanner(scanner);
-            cliRunner.run();
+            if (cliRunner.run() == 0)
+                break;
         }
     }
 }
--- src/prototype/cli/CLIRunner.java
+++ src/prototype/cli/CLIRunner.java
@@ -1,5 +1,6 @@
 package prototype.cli;
 
+import prototype.command.ExecuteCommand;
 import prototype.prompt.Prompter;
 
 import java.util.Scanner;
@@ -12,14 +13,29 @@ public class CLIRunner {
         this.prompter = new Prompter();
     }
 
+    public void printOptions() {
+        System.out.println("Select a process by entering the index number:");
+        System.out.println("1. Add a book");
+        System.out.println("2. Delete a book");
+        System.out.println("3. Add a book copy");
+        System.out.println("4. Delete a book copy");
+        System.out.println("5. Add a customers");
+        System.out.println("6. Delete a customer");
+        System.out.println("7. Borrowing a book copy");
+        System.out.println("8. Returning a book copy");
+        System.out.println("9. Exit");
+    }
+
     // Example usage
-    public void run() {
+    public int run() {
         prompter.displayPrompt("->");
         String input = prompter.getInput();
+        if (input.equals("9"))
+            return (0);
         if (!input.trim().isEmpty()) {
-            System.out.println(input);
+            ExecuteCommand executeCommand = new ExecuteCommand(Integer.parseInt(input));
         }
-
+        return (1);
     }
 
     public Prompter getPrompter() {
