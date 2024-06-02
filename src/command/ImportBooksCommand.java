cd5deade9dd4b2b424b2520235367169bbdaa3e0
Dharmang Pambhar <ge48tid@tum.de>
2024-06-02 12:49:56 +0200

    Added command class to import books from CSV file

--- src/command/ImportBooksCommand.java
+++ src/command/ImportBooksCommand.java
@@ -1,2 +1,40 @@
-package command;public class ImportBooksCommand {
+package command;
+
+import library.Book;
+import library.Library;
+import prompt.Prompter;
+import utils.CSVHelper;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ImportBooksCommand implements Command{
+    private Prompter prompter;
+    private Library library;
+
+    public ImportBooksCommand(Prompter prompter, Library library) {
+        this.prompter = prompter;
+        this.library = library;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Enter the path to the CSV file with books: ");
+        String filePath = prompter.getInput();
+        try {
+            List<String[]> lines = CSVHelper.readCSV(filePath);
+            for (String[] line: lines) {
+                String ISBN = line[0];
+                String title = line[1];
+                String author = line[2];
+                String genre = line[3];
+                String publisher = line[4];
+                Book book = new Book(title, author, genre, publisher, ISBN);
+                library.addBook(book);
+            }
+            System.out.println("Books imported successfully.");
+        } catch (IOException e) {
+            System.out.println("Failed to import books: " + e.getMessage());
+        }
+    }
 }
