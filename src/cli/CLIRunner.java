d92df802284f4b3846952730467502570999cd48
Rajkumar Pambhar <go72jon@mytum.de>
2024-06-02 13:40:34 +0200

    Integrated all new commands to the CLI

--- src/cli/CLIRunner.java
+++ src/cli/CLIRunner.java
@@ -22,33 +22,45 @@ public class CLIRunner {
 
     private void initializeCommands() {
         commandMap = new HashMap<>();
-        commandMap.put(1, new AddBookCommand(prompter, library));
+        commandMap.put(1, new ImportBooksCommand(prompter, library));
         commandMap.put(2, new DeleteBookCommand(prompter, library));
-        commandMap.put(3, new AddBookCopyCommand(prompter, library));
+        commandMap.put(3, new ImportBookCopiesCommand(prompter, library));
         commandMap.put(4, new DeleteBookCopyCommand(prompter, library));
-        commandMap.put(5, new AddCustomerCommand(prompter, library));
+        commandMap.put(5, new ImportCustomersCommand(prompter, library));
         commandMap.put(6, new DeleteCustomerCommand(prompter, library));
-        commandMap.put(7, new BorrowBookCopyCommand(prompter, library));
-        commandMap.put(8, new ReturnBookCopyCommand(prompter, library));
+        commandMap.put(7, new SearchBookCopiesCommand(prompter, library));
+        commandMap.put(8, new BorrowBookCopyCommand(prompter, library));
+        commandMap.put(9, new ReturnBookCopyCommand(prompter, library));
+        commandMap.put(10, new ReportAllBooksCommand(library));
+        commandMap.put(11, new ReportBorrowedBookCopiesCommand(library));
+        commandMap.put(12, new ReportNonBorrowedBookCopiesCommand(library));
+        commandMap.put(13, new ReportAllCustomersCommand(library));
+        commandMap.put(14, new ReportBorrowedBookCopiesByCustomerCommand(prompter, library));
     }
 
     public void printOptions() {
         System.out.println("Select a process by entering the index number:");
-        System.out.println("1. Add a book");
+        System.out.println("1. Import a file with books");
         System.out.println("2. Delete a book");
-        System.out.println("3. Add a book copy");
+        System.out.println("3. Import a file with book copies");
         System.out.println("4. Delete a book copy");
-        System.out.println("5. Add a customers");
+        System.out.println("5. Import a file with customers");
         System.out.println("6. Delete a customer");
-        System.out.println("7. Borrowing a book copy");
-        System.out.println("8. Returning a book copy");
-        System.out.println("9. Exit");
+        System.out.println("7. Search for book copies");
+        System.out.println("8. Borrow a book copy");
+        System.out.println("9. Return a book copy");
+        System.out.println("10. Report: Output of all books");
+        System.out.println("11. Report: Output of all borrowed book copies");
+        System.out.println("12. Report: Output of all non-borrowed book copies");
+        System.out.println("13. Report: Output of all customers");
+        System.out.println("14. Report: Output of all currently borrowed book copies of a customer");
+        System.out.println("15. Exit");
     }
 
     public int run() {
         prompter.displayPrompt("->");
         String input = prompter.getInput().trim();
-        if (input.equals("9"))
+        if (input.equals("15"))
             return (0);
         try {
             int commandNumber = Integer.parseInt(input);
