30dc44139dd39950db2a6463678d3e47098fa83b
Rajkumar Pambhar <go72jon@mytum.de>
2024-06-02 13:28:45 +0200

    Added command class to search for book copies

---
+++ src/command/SearchBookCopiesCommand.java
@@ -0,0 +1,37 @@
+package command;
+
+import library.Book;
+import library.BookCopy;
+import library.Library;
+import prompt.Prompter;
+
+import java.util.List;
+
+public class SearchBookCopiesCommand implements Command {
+    private Prompter prompter;
+    private Library library;
+
+    public SearchBookCopiesCommand(Prompter prompter, Library library) {
+        this.prompter = prompter;
+        this.library = library;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Enter search criteria (ISBN, Title, Author):");
+        String criteria = prompter.getInput().toLowerCase();
+        List<Book> books = library.getBooks();
+        for (Book book : books) {
+            if (book.getTitle().toLowerCase().contains(criteria) ||
+                    book.getAuthor().toLowerCase().contains(criteria)) {
+                for (BookCopy copy : book.getCopies()) {
+                    System.out.println("Title: " + book.getTitle() +
+                            ", Author: " + book.getAuthor() +
+                            ", ISBN: " + book.getISBN() +
+                            ", Copy ID: " + copy.getId() +
+                            ", Borrowed: " + copy.isBorrowed());
+                }
+            }
+        }
+    }
+}
